06d2f2641ab320c7e75802bc9cad0929791ea310
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-24 13:57:49 -0700

    DEBUG: testing debug dev tools; code is fully functional
    Co-authored-by: Jesse White <jesse.white6@gmail.com>

--- js/js-madlibs-debug.js
+++ js/js-madlibs-debug.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $("#formOne").submit(function () {
+    event.preventDefault();
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput = $("input#animal").val();
@@ -7,7 +8,7 @@ $(document).ready(function () {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
+    $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
