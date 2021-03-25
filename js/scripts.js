551ccefcb9ded99d74407958c6105896834eef36
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-25 08:47:57 -0700

    BROKEN: experiment with element targeting for individual cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-$(document).ready(function () {
-  $("div.card").click(function () {
-    $("p.term").toggle();
-    $("p.def").toggle();
-  });
+function handler(event) {
+  let target = $(event.target);
+  if (target.is("div.card-body")) {
+    target.children().css("display", "show");
+  };
+};
+
+// The toggle function for card flipping
+// $(document).ready(function () {
+//   $(".card-body").hover(function () {
+//     $(this).click(function () {
+//       let hover = $
+//       $("p.term").toggle();
+//       $("p.def").toggle();
+//     });
+//   });
 
   // $("button#yellow").click(function () {
   //   $("body").addClass("yellow-background");
@@ -11,4 +22,4 @@ $(document).ready(function () {
   // $("button#red").click(function () {
   //   $("body").addClass("red-background");
   // });
-});
+// });
