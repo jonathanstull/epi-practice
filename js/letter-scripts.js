223c5825456a90e0a75543bf7c328ec89f3f8d32
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-25 20:59:21 -0700

    add form submission function with text injection into letter

--- js/letter-scripts.js
+++ js/letter-scripts.js
@@ -1,2 +1,27 @@
 $(document).ready(function() {
-  $(
+  $("#newContact").submit(function(event) {
+    const date = $("input#date").val()
+    const account = $("input#account").val()
+    const newEmail = $("input#newEmail").val()
+    const newPhone = $("input#newPhone").val()
+    const newStreet = $("input#newStreet").val()
+    const newCity = $("input#newCity").val()
+    const newState = $("input#newState").val()
+    const newZip = $("input#newZip").val()
+    const signature = $("input#signature").val()
+
+    $(".dateIn").text(date)
+    $(".accountIn").text(account)
+    $(".newEmailIn").text(newEmail)
+    $(".newPhoneIn").text(newPhone)
+    $(".newStreetIn").text(newStreet)
+    $(".newCityIn").text(newCity)
+    $(".newStateIn").text(newState)
+    $(".newZipIn").text(newZip)
+    $(".signatureIn").text(signature)
+
+    $(".letter").show()
+
+    event.preventDefault()
+  })
+})
